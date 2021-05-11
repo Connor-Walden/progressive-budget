75a5fbe10561a6f2c00d1b905ee4f6cc4e5d8ea8
Connor-Walden <connorwalden1403@gmail.com>
2021-05-11 15:21:21 +0930

    updating to mongodb atlas

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb+srv://user:VISlKr2eZc16bA58@budgetdb.unbsc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
